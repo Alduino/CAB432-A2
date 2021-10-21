59f1d925c3634da9f722508bab080ee72f9be3cb
Alduino <git@alduino.dev>
2021-10-21 15:41:13 +1000

    Finish implementing the mockup

--- src/components/Container.tsx
+++ src/components/Container.tsx
@@ -1,10 +1,14 @@
 import {Flex, Stack} from "@chakra-ui/react";
+import Head from "next/head";
 import {ReactNode} from "react";
 import {NavBar} from "./NavBar";
 
 export function Container({children}: {children: ReactNode}) {
     return (
-        <Flex justifyContent="center" p={12}>
+        <Flex minHeight="100vh" justifyContent="center" p={12}>
+            <Head>
+                <title>Did Apple just ruin an almost perfect new MacBook Pro design?</title>
+            </Head>
             <Stack maxWidth="1320px" width="full" direction="column">
                 <NavBar />
                 {children}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
-import {Heading, Stack, StackDivider, useColorModeValue} from "@chakra-ui/react";
+import {
+    Box,
+    Button,
+    Heading,
+    HStack,
+    Link,
+    Stack,
+    StackDivider,
+    Tag,
+    Text,
+    useColorModeValue
+} from "@chakra-ui/react";
 import Head from "next/head";
 import {ByLine} from "../components/ByLine";
 import {Container} from "../components/Container";
 
 export default function Index() {
-    const faviconPath = useColorModeValue("/assets/logo-black.svg", "/assets/logo-white.svg");
+    const faviconPath = useColorModeValue(
+        "/assets/logo-black.svg",
+        "/assets/logo-white.svg"
+    );
 
     return (
         <Container>
@@ -12,13 +26,35 @@ export default function Index() {
                 <link rel="icon" href={faviconPath} />
             </Head>
 
-            <Stack direction="row" divider={<StackDivider />}>
-                <Stack w="38%" p={8} spacing={4}>
+            <Stack height="full" direction="row" divider={<StackDivider />}>
+                <Stack flexGrow={1} p={8} spacing={4}>
                     <Heading size="md">
                         Did Apple just ruin an almost perfect new MacBook Pro
                         design?
                     </Heading>
                     <ByLine author="Michael Myers" published={new Date()} />
+                    <HStack>
+                        <Tag>apple</Tag>
+                        <Tag>macbook</Tag>
+                        <Tag>macbook-pro</Tag>
+                    </HStack>
+                    <Box />
+                    <HStack>
+                        <Button variant="outline" colorScheme="gray">
+                            Find similar articles
+                        </Button>
+                    </HStack>
+                    <Box flexGrow={1} />
+                    <HStack spacing={1} fontSize="sm" opacity={.8}>
+                        <Text opacity={.8}>
+                            Something broken? Read this article on
+                        </Text>
+                        <Link href="#">a-news-website.com</Link>
+                    </HStack>
+                </Stack>
+                <Stack w="62%" p={12} spacing={4}>
+                    <Text>Lorem ipsum dolor sit amet, ...</Text>
+                    <Text>I&rsquo;m not copying all that text into code.</Text>
                 </Stack>
             </Stack>
         </Container>
