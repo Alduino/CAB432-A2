8e4c24974ef3bfb25137a8921df4dfd8fdc38d5b
Alduino <git@alduino.dev>
2021-10-28 01:39:55 +1000

    Link app name to homepage

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
-import {Box, Heading, HStack} from "@chakra-ui/react";
+import {Box, Heading, HStack, Link} from "@chakra-ui/react";
+import NextLink from "next/link";
 import {ReactElement} from "react";
 import {Logo} from "./Logo";
 import {QutLogo} from "./QutLogo";
@@ -7,7 +8,11 @@ export function NavBar(): ReactElement {
     return (
         <HStack width="full" align="center" pr={12} pl={8} py={1}>
             <Logo size={8} />
-            <Heading size="sm">Artiller</Heading>
+            <Heading size="sm">
+                <NextLink href="/" passHref>
+                    <Link>Artiller</Link>
+                </NextLink>
+            </Heading>
             <Box flexGrow={1} />
             <QutLogo height={8} />
         </HStack>
