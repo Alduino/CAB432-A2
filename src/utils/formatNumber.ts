099c2ea5a1926ec28737bf2ef773369047bbe7b2
Alduino <git@alduino.dev>
2021-10-29 11:41:09 +1000

    Use toPrecision to fix floating point errors

--- src/utils/formatNumber.ts
+++ src/utils/formatNumber.ts
@@ -11,18 +11,26 @@ const postfixes = [
 ].reverse() as [number, string][];
 
 /**
- * Adds the k, M, etc postfix to the number
+ * Adds the k, M, etc postfix to the number, and rounds it to the specified
+ * significant digits.
  * @remarks Only supports positive integers
  */
-export default function formatNumber(num: number, significantDigits = 2): string {
+export default function formatNumber(
+    num: number,
+    significantDigits = 2
+): string {
     if (num === 0) return "0";
 
-    const [divisor, postfix] = postfixes.find(([divider]) => num / divider >= 0.8);
+    const [divisor, postfix] = postfixes.find(
+        ([divider]) => num / divider >= 0.8
+    );
     const divided = num / divisor;
 
-    const decimals = Math.floor(Math.log10(divided)) - significantDigits + 1;
-    const multiplier = 10 ** decimals;
-    const numString = Math.floor(divided / multiplier) * multiplier;
-
-    return numString + postfix;
+    // TODO: Parsing the value as a float and then converting it back to a string is stupid.
+    // why does toPrecision convert to scientific notation??
+    const numString = parseFloat(
+        divided.toPrecision(significantDigits)
+    ).toString();
+    const numStringWithoutZeroes = numString.replace(/\.0+$/, "");
+    return numStringWithoutZeroes + postfix;
 }
